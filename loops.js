65307006f5c7ec447f562ff287c79617dbe2ea5f
DecentraMindsCoin <decentraminds@gmail.com>
2022-07-28 23:03:13 -0700

    add a loop through array in loops component

--- loops.js
+++ loops.js
@@ -3,3 +3,10 @@ function sayHello() {
         console.log(i)
     }
 }
+// An array with some elements
+var fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+ 
+// Loop through all the elements in the array 
+for(var i=0; i<fruits.length; i++) {
+    document.write("<p>" + fruits[i] + "</p>");
+}
