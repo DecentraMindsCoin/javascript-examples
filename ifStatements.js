f6eeb70c9151f27eb4c12bf909cf2711e021e9d6
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-15 22:39:36 -0700

    add newerTest if statment

--- ifStatements.js
+++ ifStatements.js
@@ -9,3 +9,15 @@ const newTest = () => {
     }
   };
   newTest();
+  
+const newerTest = () => {
+    let number = 1;
+    if (number <= 2 && number != 1) {
+      console.log("Your number is good");
+    } else if (number >= 2) {
+      console.log("Your number is too high");
+    } else {
+      console.log("Your number is not good");
+    }
+  };
+  newerTest();
